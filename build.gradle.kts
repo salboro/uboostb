aac1f1fd73f1194f3a0ed93c809af6b28e9d588a
salboro <salboro121@gmail.com>
2022-02-24 00:48:07 +0700

    Добавил настройки и зависимости для работы с базой и jwt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val koin_version: String by project
+val exposed_version: String by project
+val postgres_version: String by project
 
 plugins {
     application
@@ -20,18 +22,34 @@ repositories {
 }
 
 dependencies {
+    //Ktor
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 
+    //Serialization
     implementation ("io.ktor:ktor-serialization:$ktor_version")
     implementation("io.ktor:ktor-gson:1.6.7")
 
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
+    //Koin
     implementation ("io.insert-koin:koin-ktor:$koin_version")
     implementation ("io.insert-koin:koin-logger-slf4j:$koin_version")
     testImplementation("io.insert-koin:koin-test-junit5:$koin_version")
     testImplementation("io.insert-koin:koin-test:$koin_version")
+
+    //Exposed
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jodatime:$exposed_version")
+
+    //Postgres
+    implementation("org.postgresql:postgresql:$postgres_version")
+
+    //Auth
+    implementation("io.ktor:ktor-auth:$ktor_version")
+    implementation("io.ktor:ktor-auth-jwt:$ktor_version")
 }
--- src/main/kotlin/ru/Application.kt
+++ src/main/kotlin/ru/Application.kt
@@ -1,25 +1,67 @@
 package ru
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.typesafe.config.ConfigFactory
 import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.features.*
 import io.ktor.gson.*
 import io.ktor.server.netty.*
+import org.jetbrains.exposed.sql.Database
 import org.koin.ktor.ext.Koin
 import org.koin.logger.SLF4JLogger
+import ru.di.module.authenticationModule
 import ru.di.module.healthCheckModule
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 @Suppress("unused")
 fun Application.module() {
+    val conf = ConfigFactory.load()
+
+    install(CallLogging)
+
     install(ContentNegotiation) {
         gson()
     }
 
     install(Koin) {
         SLF4JLogger()
-        modules(healthCheckModule)
+        modules(healthCheckModule, authenticationModule)
     }
+
+    val secret = conf.getString("jwt.secret")
+    val issuer = conf.getString("jwt.issuer")
+    val audience = conf.getString("jwt.audience")
+    val myRealm = conf.getString("jwt.realm")
+    install(Authentication) {
+        jwt("auth-jwt") {
+            realm = myRealm
+            verifier(
+                JWT.require(Algorithm.HMAC256(secret))
+                    .withAudience(audience)
+                    .withIssuer(issuer)
+                    .build()
+            )
+
+            validate { credential ->
+                if (credential.payload.getClaim("email").asString() != "") {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
+            }
+        }
+    }
+
+    Database.connect(
+        url = conf.getString("database.databaseUrl"),
+        driver = conf.getString("database.databaseDriver"),
+        user = conf.getString("database.databaseUser"),
+        password = conf.getString("database.databasePassword")
+    )
 
     routes()
 }
