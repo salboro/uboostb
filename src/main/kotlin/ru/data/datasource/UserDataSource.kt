086c189e3115cf9fbb46dcf7d1616e84ed5791dd
salboro <salboro121@gmail.com>
2022-03-07 19:51:43 +0700

    Добавил возможность получения id пользователя только по емайлу

--- src/main/kotlin/ru/data/datasource/UserDataSource.kt
+++ src/main/kotlin/ru/data/datasource/UserDataSource.kt
@@ -1,6 +1,8 @@
 package ru.data.datasource
 
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import ru.data.dao.UserDao
 import ru.domain.entity.authentication.User
@@ -11,12 +13,12 @@ interface UserDataSource {
     fun add(user: User): Int
 
     fun getId(user: UserLogin): Int
+
+    fun getId(email: String): Int
 }
 class UserDataSourceImpl : UserDataSource {
 
     override fun add(user: User): Int = transaction {
-        addLogger(Slf4jSqlDebugLogger)
-
         UserDao.insert {
             it[name] = user.name
             it[password] = user.password
@@ -25,9 +27,11 @@ class UserDataSourceImpl : UserDataSource {
     }
 
     override fun getId(user: UserLogin): Int = transaction {
-        addLogger(Slf4jSqlDebugLogger)
-
         UserDao.select { UserDao.email.eq(user.email) and UserDao.password.eq(user.password) }.single()[UserDao.id]
+    }
+
+    override fun getId(email: String): Int = transaction {
+        UserDao.select { UserDao.email eq email }.single()[UserDao.id]
     }
 }
 
