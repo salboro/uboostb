ed534507e3f8510e3f7410ef044eb2dfefb46cf7
salboro <salboro121@gmail.com>
2022-03-13 02:17:11 +0700

    Добавил абстракции для удобного взаимодействия с bcrypt и максимальное количество символов в пароле в бд

--- src/main/kotlin/ru/data/dao/UserDao.kt
+++ src/main/kotlin/ru/data/dao/UserDao.kt
@@ -6,7 +6,7 @@ object UserDao : Table(name = "application_user") {
 
     val id = integer("id").autoIncrement().uniqueIndex()
     val name = varchar("user_name", length = 15)
-    val password = varchar("user_password", length = 15)
+    val password = varchar("user_password", length = 80)
     val email = varchar("user_email", length = 55).uniqueIndex()
 
     override val primaryKey = PrimaryKey(id)
