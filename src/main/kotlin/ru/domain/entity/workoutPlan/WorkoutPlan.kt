6973d41c1d4a05489428580649001126473cf33f
salboro <salboro121@gmail.com>
2022-04-15 22:26:43 +0700

    Поправил название поля в domain сущности WorkoutPlan

--- src/main/kotlin/ru/domain/entity/workoutPlan/WorkoutPlan.kt
+++ src/main/kotlin/ru/domain/entity/workoutPlan/WorkoutPlan.kt
@@ -2,7 +2,7 @@ package ru.domain.entity.workoutPlan
 
 data class WorkoutPlan(
     val id: Int,
-    val name: String,
+    val title: String,
     val description: String,
     val imageUri: String,
     val workouts: List<WorkoutInWorkoutPlan>,
