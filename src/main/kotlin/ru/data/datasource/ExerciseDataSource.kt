66c35229dd5ebf677ecad4ae3fcdd3d9bf0cbee3
salboro <salboro121@gmail.com>
2022-04-15 22:26:14 +0700

    Добавил получение списка упражнений из БД

---
+++ src/main/kotlin/ru/data/datasource/ExerciseDataSource.kt
@@ -0,0 +1,24 @@
+package ru.data.datasource
+
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import ru.data.dao.ExerciseDao
+import ru.domain.entity.exercise.Exercise
+
+interface ExerciseDataSource {
+
+    fun getList(): List<Exercise>
+}
+
+class ExerciseDataSourceImpl : ExerciseDataSource {
+
+    override fun getList(): List<Exercise> = transaction {
+        ExerciseDao.selectAll().map { row ->
+            Exercise(
+                row[ExerciseDao.id],
+                row[ExerciseDao.name],
+                row[ExerciseDao.iconUri] ?: ""
+            )
+        }
+    }
+}
